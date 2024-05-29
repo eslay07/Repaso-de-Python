4cd78206be001635249701860d119f68bf70e88f
Jimmy Toapanta <65260505+eslay07@users.noreply.github.com>
2024-05-29 02:47:48 -0500

    Libreria de arreglos
    Aquí comenzamos agregando la librería para los arreglos y definiendo la clase libros.

---
+++ LIBRERIA ARREGLOS_REPASO.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#creacion de programa para Almacenar la lista de libros en un arreglo:
+
+from array import array
+
+class Libro:
+    def crear_libro()
+    
+
+
+# In[ ]:
+
+
+
+
